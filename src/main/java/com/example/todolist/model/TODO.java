f201b9d9373dfbc2340f3c78d99d4228af2db038
solinMohammadi <solin8898@gmail.com>
2025-10-02 16:33:59 +0330

    todo  enity

---
+++ src/main/java/com/example/todolist/model/TODO.java
@@ -0,0 +1,67 @@
+package com.example.todolist.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class TODO {
+    private  int id;
+    private String title;
+    private  String description ;
+    private  Boolean copleted;
+    private LocalDateTime creationDate ;
+    private  LocalDateTime updateDate;
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCopleted() {
+        return copleted;
+    }
+
+    public void setCopleted(Boolean copleted) {
+        this.copleted = copleted;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+}
